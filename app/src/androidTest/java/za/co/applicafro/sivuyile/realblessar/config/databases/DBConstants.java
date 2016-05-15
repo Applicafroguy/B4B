8ae46f455c339aab35ce4fd6f6f582340e57054f
Sivuyile Magutywa <s.magutywa@applicafro.co.za>
2016-05-15 18:37:19 +0200

    fisrt commit

---
+++ app/src/androidTest/java/za/co/applicafro/sivuyile/realblessar/config/databases/DBConstants.java
@@ -0,0 +1,9 @@
+package za.co.applicafro.sivuyile.realblessar.config.databases;
+
+/**
+ * Created by sivuyile on 5/15/16.
+ */
+public class DBConstants {
+    public static final String DATABASE_NAME="blesserdb";
+    public static final int DATABASE_VERSION=1;
+}
---
+++ signup/src/main/java/za/co/applicafro/sivuyile/signup/SignupActivity.java
@@ -0,0 +1,13 @@
+package za.co.applicafro.sivuyile.signup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
