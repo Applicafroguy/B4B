f487ad00653627756a920cdb67007fc78a14dc06
Sivuyile Magutywa <s.magutywa@applicafro.co.za>
2016-05-16 03:19:59 +0200

    added acount module

--- app/src/main/java/za/co/applicafro/sivuyile/realblessar/MainActivity.java
+++ app/src/main/java/za/co/applicafro/sivuyile/realblessar/MainActivity.java
@@ -1,5 +1,6 @@
 package za.co.applicafro.sivuyile.realblessar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import za.co.applicafro.sivuyile.signup.SignupActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +43,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    // clicking signup button
+    public void onClickSignUp(View view){
+        Intent intent =new Intent(this,SignupActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -87,10 +96,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
